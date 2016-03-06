a4f7261572af2dbf82bee643590a55580d17a99f
Bo <u5495054@anu.edu.au>
2016-03-06 22:04:03 +1100

    create the project

---
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bo.comp6442_assignment_1_2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
