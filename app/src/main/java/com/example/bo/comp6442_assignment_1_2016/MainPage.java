f0067c0c1427237daf8b6bc601268917dba3d052
Bo <u5495054@anu.edu.au>
2016-03-12 17:40:50 +1100

    create new page

---
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainPage.java
@@ -0,0 +1,13 @@
+package com.example.bo.comp6442_assignment_1_2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
