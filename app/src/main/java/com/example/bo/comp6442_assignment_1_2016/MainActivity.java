a3534c6c4567158e50a2f6922f63a1bbe9dade21
Bo <u5495054@anu.edu.au>
2016-03-18 20:35:53 +1100

    construct first UI

--- app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainActivity.java
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.bo.comp6442_assignment_1_2016;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void create(View view){
+        Intent intent = new Intent(MainActivity.this,MainPage.class);
+        startActivity(intent);
     }
 }
