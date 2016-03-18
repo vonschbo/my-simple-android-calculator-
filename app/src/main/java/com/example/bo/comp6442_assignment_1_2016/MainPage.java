5c447f8232d84e60a8e72020373e44f6020a35eb
Bo <u5495054@anu.edu.au>
2016-03-18 20:40:23 +1100

    modify UI

--- app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainPage.java
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainPage.java
@@ -2,6 +2,7 @@ package com.example.bo.comp6442_assignment_1_2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    public void save(View view){
+
+    }
+
+    public void edit(View view){
+
     }
 }
