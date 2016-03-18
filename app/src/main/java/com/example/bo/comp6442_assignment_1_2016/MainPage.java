e2045be4254ef755d3624c439c87aa12962e25b4
Bo <u5495054@anu.edu.au>
2016-03-18 20:54:43 +1100

    hope no bugs

--- app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainPage.java
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainPage.java
@@ -1,20 +1,72 @@
 package com.example.bo.comp6442_assignment_1_2016;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainPage extends AppCompatActivity {
 
+    EditText textEnteredTitle;
+    EditText textEnteredContent;
+    String saved;
+    String filename;
+    FileInputStream inputStream;
+    FileOutputStream outputStream;
+    File persistentFile;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        textEnteredTitle = (EditText) findViewById(R.id.editText_title);
+        textEnteredContent = (EditText)findViewById(R.id.editText_contents);
+        filename = textEnteredTitle.getText().toString();
+
+        /* Read data from the persistent file */
+        persistentFile = new File(getFilesDir(),filename);
+        if (persistentFile.exists()){
+            try{
+                inputStream = openFileInput(filename);
+                BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                StringBuilder buffer = new StringBuilder();
+                while ((line = input.readLine()) != null){
+                    buffer.append(line);
+                }
+                saved = buffer.toString();
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void save(View view){
+        String textBuffer = saved;
+        String appendText = textEnteredContent.getText().toString();
+        textBuffer += "..";
+        textBuffer += appendText;
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(textBuffer.getBytes());
+            outputStream.close();
+            textEnteredContent.setText(textBuffer);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
+
 
     public void edit(View view){
 
