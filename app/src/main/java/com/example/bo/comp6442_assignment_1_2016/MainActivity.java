bc4b07a470d6ac593c42d280a208fff445cd2712
Bo <u5495054@anu.edu.au>
2016-03-18 20:57:35 +1100

    list view

--- app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainActivity.java
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.mylist_view);
     }
 
     public void create(View view){
