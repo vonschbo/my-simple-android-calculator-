348f9b1c00955ec0b02b39ba2a92178b375704d0
Bo <u5495054@anu.edu.au>
2016-03-19 15:46:34 +1100

    new version

--- app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainActivity.java
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/MainActivity.java
@@ -1,19 +1,58 @@
 package com.example.bo.comp6442_assignment_1_2016;
 
+import android.annotation.TargetApi;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ListView listView;
+//    ArrayList<String> noteList = new ArrayList<String>();
+//    ListView listView;
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.mylist_view);
+//        listView = (ListView) findViewById(R.id.mylist_view);
+//        EditText EditTitle = (EditText)findViewById(R.id.editText_title);
+//
+//        ArrayAdapter<String> aa;
+//        aa = new ArrayAdapter<String>(this,R.layout.activity_main,noteList);
+//        listView.setAdapter(aa);
+        insertNote("New note");
+
+        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI,
+                mySimpleDB.ALL_COLUMNS, null, null, null, null);
+        String[] from = {mySimpleDB.NOTE_TEXT};
+        int[] to = {android.R.id.text1};
+        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,cursor, from,to,0);
+
+        ListView list = (ListView)findViewById(R.id.list1);
+        list.setAdapter(cursorAdapter);
+
+
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(mySimpleDB.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        Log.d("MainActivity", "Inserted note" + noteUri.getLastPathSegment());
     }
 
     public void create(View view){
---
+++ app/src/main/java/com/example/bo/comp6442_assignment_1_2016/NotesProvider.java
@@ -0,0 +1,69 @@
+package com.example.bo.comp6442_assignment_1_2016;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Bo on 19/03/2016.
+ */
+public class NotesProvider extends ContentProvider {
+
+    private static final String AUTHORITY = "com.example.bo.comp6442_assignment_1_2016.notesprovider";
+    private static final String BASE_PATH = "notes";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+    // Constant to identify the requested operation
+    private static final int NOTES = 1;
+    private static final int NOTES_ID = 2;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        uriMatcher.addURI(AUTHORITY,BASE_PATH,NOTES);
+        uriMatcher.addURI(AUTHORITY,BASE_PATH+"/#",NOTES_ID);
+    }
+
+    private SQLiteDatabase database;
+
+    @Override
+    public boolean onCreate() {
+        mySimpleDB myDB = new mySimpleDB(getContext());
+        SQLiteDatabase database = myDB.getWritableDatabase();
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return database.query(mySimpleDB.TABLE_NOTES,mySimpleDB.ALL_COLUMNS,selection,null,null,null,
+                mySimpleDB.NOTE_CREATED + "Descending");
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long id = database.insert(mySimpleDB.TABLE_NOTES,null,values);
+        return Uri.parse(BASE_PATH + "/" + id);
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return database.delete(mySimpleDB.TABLE_NOTES,selection,selectionArgs);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return database.update(mySimpleDB.TABLE_NOTES,values,selection,selectionArgs);
+    }
+}
